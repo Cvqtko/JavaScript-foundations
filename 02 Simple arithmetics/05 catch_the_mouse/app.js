5e349de82b6dfac6fdebc20a70cd3062272e0751
Cvqtko <cvqtkokirilov@gmail.com>
2021-01-09 10:33:23 +0200

    Exercises

---
+++ 02 Simple arithmetics/05 catch_the_mouse/app.js
@@ -0,0 +1,9 @@
+const interval = setInterval(function() {
+	let img = document.getElementById("image");
+	img.style.position = "absolute";
+	img.style.left = (Math.random() * 300) + "px";
+	img.style.top = (Math.random() * 300) + "px";
+}, 500);
+function catchMouse() {
+	alert("Congratulations, You Win!")
+}
